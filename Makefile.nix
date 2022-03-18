e8003593953d9deb0b8edbeac42dea6a9e89bf50
Devam Sisodraker <dsisodraker@coveo.com>
2022-03-18 08:46:49 -0700

    added linux compatible makefile

---
+++ Makefile.nix
@@ -0,0 +1,29 @@
+#!make
+include .env
+export
+
+default:	| setup build
+
+init:
+	cp .env.dist .env
+
+setup:
+	npm install
+
+build:
+	npm run build
+
+watch:
+	npm run watch
+
+serve:
+	./node_modules/.bin/coveops serve
+
+pack: 
+	npm pack
+
+publish:
+	npm publish --access public
+
+	
+.PHONY: default init setup build serve pack publish
